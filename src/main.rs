2933d800bb698957e1c07c669ecdc2b8aa9ac063
CheetahDoesStuff <bravestcheetah@gmail.com>
2026-03-28 16:37:47 +0100

    Tests now work!

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
+use core::panic::PanicInfo;
 mod vga_buffer;
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -15,6 +26,16 @@ fn panic(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     println!("One line");
     println!("Two line");
-    panic!("This is a fake panic to test!");
+
+    #[cfg(test)]
+    test_main();
+    
     loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
