d5ad30a75eaa6457e9de6089d17cc28f3c743515
CheetahDoesStuff <bravestcheetah@gmail.com>
2026-03-28 16:02:15 +0100

    Noice

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
@@ -10,13 +12,6 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in "Hello World!".as_bytes().iter().enumerate() {
-        unsafe {
-            *vga_buffer.add(i * 2) = byte;
-            *vga_buffer.add(i * 2 + 1) = 0x0f; 
-        }
-    }
+    vga_buffer::print_something();
     loop {}
 }
