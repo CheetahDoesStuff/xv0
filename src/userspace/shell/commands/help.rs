e7d402cecc91638e58d595ae53ba3184cf7bc1e6
CheetahDoesStuff <bravestcheetah@gmail.com>
2026-04-24 19:51:47 +0200

    Better help mesage

--- src/userspace/shell/commands/help.rs
+++ src/userspace/shell/commands/help.rs
@@ -1,16 +1,11 @@
 use crate::println;
 
 pub fn help(_: &[&str]) {
-    println!(
-        "Hello there! Welcome to the xv0 shell. Here is a list of documented commands to get you started:"
-    );
-    println!("  clear - Clear the screen");
-    println!("  help  - Show this help message");
-    println!("  commands   - List all available commands");
-    println!("  echo [string]   - Echo the input back to the console");
-    println!("  exit  - Exit the shell and shut down the system");
-    println!();
-    println!(
-        "Commands are added regularly in updates, to view all commands run 'commands', this will dynamically list all available commands."
-    );
+    println!("Hello there! Welcome to the xv0 shell");
+    println!("Here is are the documented commands:");
+    println!("  clear   - Clear the screen");
+    println!("  help    - Show this help message");
+    println!("  doom    - DOOM");
+    println!("  commands        - List commands");
+    println!("  echo [string]   - Echo input");
 }
