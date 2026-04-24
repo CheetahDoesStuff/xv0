34c8677e4b66b9f1986e270c7655a93efb552c03
CheetahDoesStuff <bravestcheetah@gmail.com>
2026-04-24 20:15:15 +0200

    Starts of a file system?

--- src/userspace/tasks/shell.rs
+++ src/userspace/tasks/shell.rs
@@ -1,10 +1,15 @@
 use crate::{print, userspace::input::keyboard_handler::read_line};
 
+static mut DIR: &str = "/";
+pub fn get_dir() -> &'static str { unsafe { DIR } }
+pub fn set_dir(new_dir: &'static str) { unsafe { DIR = new_dir; } }
+
 pub async fn shell() {
+    crate::kernel::vga_buffer::clear_screen();
     crate::println!("Welcome to the xv0 shell!");
     crate::println!("Type 'help' for a list of commands.");
     loop {
-        print!("> ");
+        print!("{} > ", get_dir());
         let input = read_line(true, true).await;
         crate::userspace::shell::command_table::dispatch(input.as_str());
     }
